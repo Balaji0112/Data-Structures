1fbfd0ffb91b55bb88a8751602f125ad0f5ce35e
Balaji0112 <60376123+Balaji0112@users.noreply.github.com>
2020-03-12 17:14:52 +0530

    selection_sort

---
+++ Selection_sort.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+class Selection_Sort
+{
+    public void sort(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            int min_element_index=i;
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[j] < arr[min_element_index])
+                {
+                    min_element_index=j;
+                }
+            }
+            int temp=arr[min_element_index];
+            arr[min_element_index]=arr[i];
+            arr[i]=temp;
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String args[])
+    {
+        Selection_Sort obj=new Selection_Sort();
+        Scanner sc=new Scanner(System.in);
+        int no_of_elements=sc.nextInt();
+        int arr[]=new int[no_of_elements];
+        for(int i=0;i<no_of_elements;i++)
+        {
+            int array_element=sc.nextInt();
+            arr[i]=array_element;
+        }
+        obj.sort(arr);
+    } 
+}
