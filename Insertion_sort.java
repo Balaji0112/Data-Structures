f5abcb12456f3d02fb7f12890fbaef5da9051a3f
Balaji0112 <60376123+Balaji0112@users.noreply.github.com>
2020-03-22 19:04:38 +0530

    Insertion_sort

---
+++ Insertion_sort.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class Insertion_sort
+{
+    void sort(int arr[])
+    {
+        for(int i=1;i<arr.length;i++)
+        {
+            int value=arr[i];
+            int x=i-1;
+            while(x>=0 && arr[x]>value)
+            {
+                arr[x+1]=arr[x];
+                x=x-1;
+            }
+            arr[x+1]=value;
+        }
+    }
+    void print(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            int arr_input=sc.nextInt();
+            arr[i]=arr_input;
+        }
+        Insertion_sort obj=new Insertion_sort();
+        obj.sort(arr);
+        obj.print(arr);
+    }
+}
