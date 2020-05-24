07335e8a8a822b00f30086a1ab5d708da8fd314c
Balaji0112 <60376123+Balaji0112@users.noreply.github.com>
2020-05-24 14:36:12 +0530

    Qsmidpiv

---
+++ QsMidPivot.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+public class QsMidPivot {
+	public static void quickSort(int[] arr, int low, int high) {
+        if (low >= high)
+        {
+           return;
+        }
+		int middle = low + (high - low)/2;
+		int pivot = arr[middle];
+		int i = low, j = high;
+		while (i <= j) {
+			while (arr[i]<pivot) {
+				i++;
+			}
+			while (arr[j]>pivot) {
+				j--;
+			}
+			if (i<=j) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				i++;
+				j--;
+			}
+		}
+		if (low<j)
+			quickSort(arr, low, j);
+		if (high>i)
+			quickSort(arr, i, high);
+    }
+    void print(int arr[])
+    {
+          for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int l=0;
+        int r=n-1;
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            int element=sc.nextInt();
+            arr[i]=element;
+        }
+        QsMidPivot obj=new QsMidPivot();
+        obj.quickSort(arr,l,r);
+        obj.print(arr);
+        // for(int i=0;i<n;i++){
+        //     System.out.print(x[i]+" ");
+        // }
+    }
+}
